533f9194514dbde0215bf57c8ca284fe20a7a068
Bryan Chasko <reddythafox@hotmail.com>
2024-12-03 16:19:50 -0800

    uncomment

--- cesium-app/public/app.js
+++ cesium-app/public/app.js
@@ -8,7 +8,7 @@ const viewer = new Cesium.Viewer('cesiumContainer');
 
 // Commented out the GeoJSON loading for now
 // Load the GeoJSON file for Puget Sound visualization
-// Cesium.GeoJsonDataSource.load('./puget_sound_data/puget_sound_data.geojson').then(function (dataSource) {
-//     viewer.dataSources.add(dataSource);
-//     viewer.flyTo(dataSource);
-// });
+ Cesium.GeoJsonDataSource.load('./puget_sound_data/puget_sound_data.geojson').then(function (dataSource) {
+     viewer.dataSources.add(dataSource);
+     viewer.flyTo(dataSource);
+ });
