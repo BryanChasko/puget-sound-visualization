d80022bdf4a48420859c224c5db805e66184392a
Bryan Chasko <reddythafox@hotmail.com>
2024-12-03 16:16:22 -0800

    updated

--- cesium-app/public/app.js
+++ cesium-app/public/app.js
@@ -4,12 +4,11 @@ Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOi
 console.log('Cesium version:', Cesium.VERSION);
 
 // Initialize the Cesium Viewer
-const viewer = new Cesium.Viewer('cesiumContainer', {
-    terrainProvider: Cesium.createWorldTerrain()
-});
+const viewer = new Cesium.Viewer('cesiumContainer');
 
+// Commented out the GeoJSON loading for now
 // Load the GeoJSON file for Puget Sound visualization
-Cesium.GeoJsonDataSource.load('./puget_sound_data/puget_sound_data.geojson').then(function (dataSource) {
-    viewer.dataSources.add(dataSource);
-    viewer.flyTo(dataSource);
-});
+// Cesium.GeoJsonDataSource.load('./puget_sound_data/puget_sound_data.geojson').then(function (dataSource) {
+//     viewer.dataSources.add(dataSource);
+//     viewer.flyTo(dataSource);
+// });
